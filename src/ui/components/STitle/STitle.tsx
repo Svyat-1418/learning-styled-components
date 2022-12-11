3c3e00498dd1bad5558016a3abe23c2879a69455
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-12-11 18:17:23 +0300

    Getting started

---
+++ src/ui/components/STitle/STitle.tsx
@@ -0,0 +1,8 @@
+// Create a Title component that'll render an <h1> tag with some styles
+import styled from "styled-components";
+
+export const STitle = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
---
+++ src/ui/components/SWrapper/SWrapper.tsx
@@ -0,0 +1,8 @@
+// Create a Wrapper component that'll render a <section> tag with some styles
+import styled from "styled-components";
+
+export const SWrapper = styled.section`
+  height: 100vh;
+  padding: 4em;
+  background: #34150d;
+`;
